c50d81a66af1e5edeebca05f873ed42f893a0afe
dinhlinh2002 <130349416+dinhlinh2002@users.noreply.github.com>
2024-05-16 00:21:55 +0700

    update code

--- Assignment1/EStore.WPF/obj/Debug/net6.0-windows/EStore.WPF_ehuubwge_wpftmp.AssemblyInfo.cs
+++ Assignment1/EStore.WPF/obj/Debug/net6.0-windows/EStore.WPF_ehuubwge_wpftmp.AssemblyInfo.cs
@@ -14,11 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EStore.WPF")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-<<<<<<< Updated upstream
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8f3d2fdd734bd77cfe06952c9b9800dd7ef5d57c")]
-=======
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6ff490a1f3678812fb599bc8c9bc32ae0f6f26a5")]
->>>>>>> Stashed changes
 [assembly: System.Reflection.AssemblyProductAttribute("EStore.WPF")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EStore.WPF")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
